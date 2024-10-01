97a43f10461a780e82b77c2cf48401e1cea4a78f
ashutosh1869 <127655895+ashutosh1869@users.noreply.github.com>
2024-10-02 02:32:07 +0530

    fix: path

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "./components/header";
-import Footer from "./components/footer";
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
 import { Outlet } from "react-router-dom";
 function Layout(){
     return(
