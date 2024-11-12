14e8f6516151fa6be3de18d45fec79bbae717775
ashutosh1869 <127655895+ashutosh1869@users.noreply.github.com>
2024-11-13 04:07:59 +0530

    fix: imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Home from './components/Home/Home.jsx'
-import Layout from './Layout'
+import Layout from './Layout.jsx'
 import About from './components/About/About.jsx'
 import './index.css'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
@@ -31,8 +31,8 @@ import Github,{githubLoader} from './components/Github/Github.jsx'
 // ])
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Layout />}>
-    <Route path='' element={<Home />} />
+    <Route path='/' element={<Layout/>}>
+    <Route path='' element={<Home/>} />
     <Route path='About' element={<About/>} />
     <Route path='contact' element={<Contact/>} />
     <Route path='user/:userid' element={<User/>} />
